d58f06f911c5b56e1c8adc40bf5762def9692b20
xiangzhu <xiang_zhu@sunrun99.com>
2017-09-11 14:26:41 +0800

    all in concurrency

---
+++ gitlife/src/main/java/com/dannysoft/gitlife/concurrency/ExcelGenerator.java
@@ -0,0 +1,10 @@
+package com.dannysoft.gitlife.concurrency;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
+public class ExcelGenerator {
+	
+	ThreadPoolExecutor executor =  (ThreadPoolExecutor)Executors.newCachedThreadPool();
+
+}
